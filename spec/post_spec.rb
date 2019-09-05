dbcfd6387313386710f05da80a4652a61eb53167
Dennis Fazio <fazioferreira@gmail.com>
2019-09-05 06:32:28 -0300

    Factory

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,15 +1,20 @@
 describe 'cadastro' do
-    it 'novo usuário' do
 
-        Database.new.delete_user("me@papito.io")
-        result = HttParty.post(
-           "http://192.168.99.100/user",
-           body: {full_name: "Fernando Papito", email: "eu@papito.io", password: 'jarvis123'}.to_json,
-           headers: {
-                'Content-Type' => 'application/json',
-           }, 
-        )
-        puts result
-        expect(result.response.code).to eql "200"
+    context "when new user" do
+        before do
+            @new_user = {full_name: "Fernando Papito", email: "eu@papito.io", password: 'jarvis123'}
+            Database.new.delete_user("me@papito.io")
+
+            @result = HttParty.post(
+                "http://192.168.99.100/user",
+                body: @new_user.to_json,
+                headers: {
+                     'Content-Type' => 'application/json',
+                }, 
+             )
+        end
+        it{expect(@result.response.code).to eql "200"}
     end
 end
+
+   
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-
 require 'httparty'
+require 'factory_bot'
 
 RSpec.configure do |config|
 
@@ -15,5 +15,6 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  config.include FactoryBot::Syntex::Methods
 
 end
